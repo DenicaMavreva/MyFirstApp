3bf214255afdc4e28524d017af30e47ead86cbfa
Denica Mavreva <denicamavreva@gmail.com>
2018-12-20 22:04:14 +0200

    Delete/Edit and so on

---
+++ CCA/ViewModels/Course/CourseInputViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCA.ViewModels.Course
+{
+    public class CourseInputViewModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
+        public string Title { get; set; }
+
+        [Required]
+        public int Credits { get; set; }
+
+        [Required]
+        [StringLength(250, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
+        public string Description { get; set; }
+
+    }
+}
---
+++ CCA/ViewModels/Course/LoggedInViewCourses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCA.ViewModels.Course
+{
+    public class LoggedInViewCourses
+    {
+        public LoggedInViewCourses()
+        {
+            this.Courses = new List<CourseIndexDto>();
+        }
+
+        public ICollection<CourseIndexDto> Courses { get; set; }
+    }
+}
